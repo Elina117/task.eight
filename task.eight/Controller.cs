de11336fb9bf73dc7f7ec7e5e9fd0b1f8c55599b
Elina117 <106472257+Elina117@users.noreply.github.com>
2022-11-18 17:38:10 +0300

    second.commit

--- task.eight/Controller.cs
+++ task.eight/Controller.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Net.NetworkInformation;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace task.eight
 {
 	public class Controller
 	{
+		
 		public static Project CreateProject(string description, DateTime deadline, Person reporter, Person teamlead, List<Task> tasks)
         {
 			string status = "ПРОЕКТ";
@@ -12,7 +14,19 @@ namespace task.eight
 			return project;
 		}
 		
+		public static Task AssignPerformer(string description, DateTime deadline, Person reporter, Person assignee, Report report)
+		{
+			string status = "НАЗНАЧЕНА";
+			Task task = new Task(description, deadline, reporter, assignee, status, report);
+            return task;
+        }
 
-	}
+		public static Report CreateReport(string text, DateTime date, Person assignee)
+		{
+			Report report = new Report(text, date, assignee);
+			return report;
+		}
+
+    }
 }
 
--- task.eight/Main.cs
+++ task.eight/Main.cs
@@ -1,10 +1,52 @@
 ﻿using System;
+
+
 namespace task.eight
 {
 	public class Program
 	{
         static void Main(string[] args)
         {
+            
+            //1 method
+            string description = "Creating a website";
+            DateTime deadline = new DateTime();
+            Person reporter = new Person("Ahmetov");
+            Person teamlead = new Person("Khuzina");
+            List<Task> tasks = new List<Task>() { };
+  
+            Project project = Controller.CreateProject(description, deadline, reporter, teamlead, tasks);
+            Console.WriteLine(project);
+
+
+            //2 method
+            Task task1 = new Task("lkvne", new DateTime(), new Person("vjb"), new Person("elvpj"), "lcnlv", new Report("lwcbc", new DateTime(), new Person("wlc")));
+            Task task2 = new Task("lfkev", new DateTime(), new Person("kfn"), new Person("ve"), "etg", new Report("vfe", new DateTime(), new Person("vefve")));
+            Task task3 = new Task("rbbbt", new DateTime(), new Person("greerg"), new Person("gtb"), "bt", new Report("vcer", new DateTime(), new Person("brgb")));
+
+            project.tasks.Add(task1);
+            project.tasks.Add(task2);
+            project.tasks.Add(task3);
+            
+            Console.WriteLine(project);
+
+
+            //3 method
+            string description = "oooo";
+            DateTime deadline = new DateTime();
+            Person reporter = new Person("tgho");
+            Person assignee = new Person("pgje");
+            string status = "jfjj";
+            Report report = new Report("ieohg", new DateTime(), new Person("ggro"));
+
+            Task task = Controller.AssignPerformer(description, deadline, reporter, assignee, report);
+
+            //4 method
+            string text = "lknvoenv";
+            DateTime date = new DateTime();
+            Person assignee = new Person("fojeofj");
+
+            Report report = Controller.CreateReport(text, date, assignee);
 
         }
 
